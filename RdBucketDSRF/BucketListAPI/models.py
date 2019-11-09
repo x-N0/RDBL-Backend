6caa7ddc788b21953b2c56bd350a93e051a31c72
Robinson <gnrn@protonmail.ch>
2019-11-09 01:20:34 -0800

    Image Field Path Integration. working OK. Tests OK.

--- RdBucketDSRF/BucketListAPI/models.py
+++ RdBucketDSRF/BucketListAPI/models.py
@@ -4,10 +4,16 @@ from django.db import models
 # Create your models here.
 
 class BucketList(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=False, unique=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+    ImgPath = models.ImageField(upload_to="secured/", null=True, blank=True)
 
     def __str__(self):
         """Return a string representation of the model instance."""
         return "{}".format(self.name)
+
+
+"""class PhotoBucketList(models.Model):
+    Bucket = models.OneToOneField(BucketList, on_delete=models.CASCADE)"""
--- RdBucketDSRF/BucketListAPI/urls.py
+++ RdBucketDSRF/BucketListAPI/urls.py
@@ -1,11 +1,13 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
-
 from .views import *
 
-urlpatterns = {
+urlpatterns = [
     url(r'^bucketlists/$', BucketListCreationView.as_view(), name='Create Bucket'),
-    url(r'^bucketlists/(?P<pk>\d+)/$', BucketListGenView.as_view(), name='Bucket Mod')
-}
-urlpatterns = format_suffix_patterns(urlpatterns)
+    url(r'^bucketlists/(?P<pk>\d+)/$', BucketListGenView.as_view(), name='Bucket Mod'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
